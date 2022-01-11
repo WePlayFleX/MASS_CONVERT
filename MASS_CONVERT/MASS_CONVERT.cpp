a01590c3479bf53dba48ec0b743ceffe69c83cc0
WePlayFleX <alex.kamai@ya.ru>
2022-01-11 23:32:21 +0300

    Добавьте файлы проекта.

---
+++ MASS_CONVERT/MASS_CONVERT.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+float lbstokg(float);
+
+int main()
+{
+	float lbs, kgs;
+	cout << "Enter weight in pounds: ";
+	cin >> lbs;
+	kgs = lbstokg(lbs);
+	cout << "Weight in kilograms is " << kgs << endl;
+
+	system("pause");
+	return 0;
+}
+
+float lbstokg(float pounds)
+{
+	float kilograms = 0.453592 * pounds;
+	return kilograms;
+}
