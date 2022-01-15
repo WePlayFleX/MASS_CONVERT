27729129ef4bd18cfc276c641c6908c63ecd7c0c
WePlayFleX <alex.kamai@ya.ru>
2022-01-15 23:29:11 +0300

    V1

--- MASS_CONVERT/MASS_CONVERT.cpp
+++ MASS_CONVERT/MASS_CONVERT.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-float lbstokg(float);
+//float lbstokg(float);
+
+inline float lbstokg(float pounds)
+{
+	return 0.453592 * pounds;
+}
 
 int main()
 {
@@ -11,10 +16,12 @@ int main()
 	cout << "Enter weight in pounds: ";
 	cin >> lbs;
 
+	cout << " " << lbstokg(lbs) << endl;
+
 	system("pause");
 	return 0;
 }
 
-float lbstokg(float pounds)
-{
-}
+//float lbstokg(float pounds)
+//{
+//}
